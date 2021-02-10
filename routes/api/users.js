0ec34f5d566f83ba10f4bdff40b3ec15a347c3d2
akash <akash.ry@somaiya.edu>
2021-02-10 12:44:57 +0530

    login Validation with error messages

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport')
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 // Load User model
 const User = require('../../models/User')
@@ -70,6 +71,13 @@ router.post('/register',(req,res) =>{
 // @desc Login User / Returning JWT token
 // @access public
 router.post('/login', (req,res) =>{
+    const { errors, isValid} = validateLoginInput(req.body)
+
+    // Check Validation
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+    
     const email = req.body.email
     const password = req.body.password
 
@@ -78,7 +86,8 @@ router.post('/login', (req,res) =>{
         .then(user => {
             // Check for user
             if(!user) {
-                return res.status(404).json({email:'User not found'})
+                errors.email = 'User not found'
+                return res.status(404).json(errors)
             }
 
             // Check password
@@ -106,7 +115,8 @@ router.post('/login', (req,res) =>{
                             }
                         )
                     }else{
-                        return res.status(400).json({password: 'Password Incorrect'})
+                        errors.password = 'Password Incorrect'
+                        return res.status(400).json(errors)
                     }
                 })
         })
