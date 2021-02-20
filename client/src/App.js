709445e75c6b3925cb6b2cbb7c1e721e88efb8d0
akash <akash.ry@somaiya.edu>
2021-02-20 13:09:01 +0530

    basic bootstrap layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
 
 function App() {
   return (
     <div className="App">
-      <h1>My react app</h1>
+      <Navbar/>
+      {/* <div className="container"></div> */}
+      <Landing/>
+      <Footer/>
     </div>
   );
 }
