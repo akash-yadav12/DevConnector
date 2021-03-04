c2f2992bb0818e30113ac81c8b7ab77f9c304991
akash <akash.ry@somaiya.edu>
2021-03-04 20:05:33 +0530

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,28 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store'
 
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-          <Navbar/>
-          <Route exact path="/" component={Landing}/>
-          <div class="container">
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
-          </div>
-          <Footer/>
-      </div>
-    </Router>
-    
+    <Provider store={ store }>
+      <Router>
+        <div className="App">
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
+            <div class="container">
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+            </div>
+            <Footer/>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
