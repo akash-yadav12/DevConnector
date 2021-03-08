1a03e1766e9b9a05044e4f36059277810cb64f52
akash <akash.ry@somaiya.edu>
2021-03-08 21:19:04 +0530

    loading spinner add dashboard, profile redux:

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,17 +1,18 @@
 import axios from 'axios'
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE} from './types'
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE} from './types'
 
 // Get current profile 
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading)
   axios.get('/api/profile')
-    .then(res => 
+    .then(res => {
+      console.log(res.data)
       dispatch({
         type: GET_PROFILE,
         payload: res.data
       })  
-    )
+    })
     .catch(err => 
       dispatch({
         type:GET_PROFILE,
