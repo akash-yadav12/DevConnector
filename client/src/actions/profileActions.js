2a478158c0a5108c732dc48a2e38f8ec768a220e
akash <akash.ry@somaiya.edu>
2021-03-10 13:26:43 +0530

    Delete account frontend

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS} from './types'
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER} from './types'
 
 // Get current profile 
 export const getCurrentProfile = () => dispatch => {
@@ -31,6 +31,27 @@ export const createProfile = (profileData, history) => dispatch => {
         payload: err.response.data
       })
     )
+}
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if(window.confirm('Are you sure? This cannot be undone!')){
+    axios
+      .delete('/api/profile')
+      .then(res => 
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err => 
+        dispatch({
+          type:GET_ERRORS,
+          payload: err.response.data
+        })
+      )
+
+  }
 }
 
 // Profile laoding
